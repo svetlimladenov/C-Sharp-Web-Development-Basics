4acebcbeb695f3e6b62982fd1959ed95c347448c
Svetlin Mladenov <svetlimladenov@gmail.com>
2018-10-15 03:41:15 +0300

    Small improvements on IRunesWebApp.

---
+++ SIS – SoftUni Information Services/SIS/Apps/CakesWebApp/ViewModels/Cakes/DoAddCakesModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CakesWebApp.ViewModels.Cakes
+{
+    public class DoAddCakesModel
+    {
+        public string Name { get; set; }
+
+        public string Price { get; set; }
+
+        public string Picture { get; set; }
+
+    }
+}
---
+++ SIS – SoftUni Information Services/SIS/Apps/IRunesWebApp/ViewModels/Tracks/AddTrackInputModel.cs
@@ -0,0 +1,14 @@
+﻿namespace IRunesWebApp.ViewModels.Tracks
+{
+    public class AddTrackInputModel
+    {
+        public string TrackName { get; set; }
+
+        public string TrackLink { get; set; }
+
+        public string TrackPrice { get; set; }
+
+        public string AlbumId { get; set; }
+
+    }
+}
