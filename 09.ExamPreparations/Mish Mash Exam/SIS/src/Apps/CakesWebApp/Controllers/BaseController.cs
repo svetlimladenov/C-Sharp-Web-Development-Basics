f750fcbf6d5ca5de9d11f198975ffa972dc22898
Svetlin Mladenov <svetlimladenov@gmail.com>
2018-10-23 23:55:01 +0300

    Added Exam Preparation directory

---
+++ 09.ExamPreparations/Mish Mash Exam/SIS/src/Apps/CakesWebApp/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using CakesWebApp.Data;
+using SIS.HTTP.Enums;
+using SIS.HTTP.Requests;
+using SIS.HTTP.Responses;
+using SIS.MvcFramework;
+using SIS.MvcFramework.Services;
+
+namespace CakesWebApp.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        protected BaseController()
+        {
+            this.Db = new CakesDbContext();
+        }
+
+        protected CakesDbContext Db { get; }
+    }
+}
---
+++ 09.ExamPreparations/Mish Mash Exam/SIS/src/Apps/CakesWebApp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using SIS.HTTP.Enums;
+using SIS.HTTP.Requests;
+using SIS.HTTP.Responses;
+using SIS.MvcFramework;
+using SIS.MvcFramework.Services;
+
+namespace CakesWebApp.Controllers
+{
+    public class HomeController : BaseController
+    {
+        [HttpGet("/")]
+        public IHttpResponse Index()
+        {
+            return this.View("Index");
+        }
+
+        [HttpGet("/hello")]
+        public IHttpResponse HelloUser()
+        {
+            return this.View("HelloUser", new HelloUserViewModel { Username = this.User });
+        }
+    }
+
+    public class HelloUserViewModel
+    {
+        public string Username { get; set; }
+    }
+}
